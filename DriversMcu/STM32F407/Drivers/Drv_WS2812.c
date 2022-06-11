168f3e3ee104b74ab5b80e07711a6c4d58799b72
Ellu <31679332+ElluIFX@users.noreply.github.com>
2022-06-11 21:59:59 +0800

    ✨ feat(外设驱动): 新增ws2812驱动模块
    不使用DMA和定时器，故不适合驱动大量ws2812

---
+++ DriversMcu/STM32F407/Drivers/Drv_WS2812.c
@@ -0,0 +1,179 @@
+/**
+ * @file Drv_WS2812.c
+ * @brief Driver for WS2812 RGB LED
+ * @author Ellu (lutaoyu@163.com)
+ * @version 1.0
+ * @date 2022-06-11
+ *
+ * THINK DIFFERENTLY
+ */
+
+#include "Drv_WS2812.h"
+static uint8_t ws2812_buf[3 * WS2812_NUM + 1];
+static uint8_t ws2812_send_flag = 0;
+
+/**
+ * @brief Initialize WS2812 GPIO and buffer
+ */
+void DrvWS2812Init(void) {
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_StructInit(&GPIO_InitStructure);
+
+  RCC_AHB1PeriphClockCmd(__WS2812_RCC, ENABLE);
+
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Pin = __WS2812_PIN;
+  GPIO_Init(__WS2812_GPIO, &GPIO_InitStructure);
+
+  GPIO_ResetBits(__WS2812_GPIO, __WS2812_PIN);
+
+  for (u8 i = 0; i < WS2812_NUM; i++) {
+    ws2812_buf[3 * i + 0] = 0;
+    ws2812_buf[3 * i + 1] = 0;
+    ws2812_buf[3 * i + 2] = 0;
+  }
+  WS2812_SendBit(ws2812_buf, WS2812_NUM);
+}
+
+#pragma GCC push_options
+#pragma GCC optimize("O0")
+/**
+ * @brief Send data bits to WS2812
+ * @param  data             uint32_t LED array, each in G R B order
+ * @param  len              the number of LEDs
+ */
+void WS2812_SendBit(uint8_t* data, uint8_t len) {
+  static uint8_t _i = 0;
+  static uint8_t _j = 0;
+  for (_i = 0; _i < len * 3; _i++) {
+    for (_j = 0; _j < 8; _j++) {
+      if (*data & (128 >> _j)) {
+        __WS2812_HIGH_BIT;
+      } else {
+        __WS2812_LOW_BIT;
+      }
+    }
+    data++;
+  }
+}
+#pragma GCC pop_options
+
+/**
+ * @brief Set specific LED to ARGB color
+ * @param  argb_data        uint32_t ARGB color (Alpha, Red, Green, Blue)
+ * @param  index            the index of LED (0 - WS2812_NUM)
+ */
+void WS2812_SetARGB(uint32_t argb_data, uint8_t index) {
+  if (index >= WS2812_NUM) {
+    return;
+  }
+  uint16_t brightness = (uint16_t)(argb_data >> 24 & 0xFF);
+  uint8_t r = (argb_data >> 8) & 0xFF;
+  uint8_t g = (argb_data >> 16) & 0xFF;
+  uint8_t b = argb_data & 0xFF;
+  ws2812_buf[3 * index] = (r * brightness) / 255;
+  ws2812_buf[3 * index + 1] = (g * brightness) / 255;
+  ws2812_buf[3 * index + 2] = (b * brightness) / 255;
+}
+
+/**
+ * @brief Set specific LED to RGB color, alpha is set to 255
+ * @param  argb_data        uint32_t RGB color (Red, Green, Blue)
+ * @param  index            the index of LED (0 - WS2812_NUM)
+ */
+void WS2812_SetRGB(uint32_t rgb_data, uint8_t index) {
+  WS2812_SetARGB(rgb_data | 0xFF000000, index);
+}
+
+/**
+ * @brief Send data bits to WS2812 immediately.
+ * This function is not thread safe. Will disable interrupts
+ * during transmission.
+ */
+void WS2812_ForceSendBuf(void) {
+  __disable_irq();  // disable all interrupts
+  WS2812_SendBit(ws2812_buf, WS2812_NUM);
+  __enable_irq();  // enable all interrupts
+  ws2812_send_flag = 0;
+}
+
+/**
+ * @brief Enable WS2812 send flag, sending will be started by
+ * WS2812_SendBufChecker.
+ */
+void WS2812_SendBuf(void) { ws2812_send_flag = 1; }
+
+/**
+ * @brief Check if WS2812 send flag is set, call in where this function won't be
+ * blocked.
+ */
+void WS2812_SendBufChecker(void) {
+  if (ws2812_send_flag) {
+    WS2812_SendBit(ws2812_buf, WS2812_NUM);
+    ws2812_send_flag = 0;
+  }
+}
+
+/**
+ * @brief Set all LEDs to one ARGB color
+ * @param  argb_data        uint32_t ARGB color (Alpha, Red, Green, Blue)
+ */
+void WS2812_SetAll(uint32_t argb_data) {
+  for (uint8_t i = 0; i < WS2812_NUM; i++) {
+    WS2812_SetARGB(argb_data, i);
+  }
+}
+
+/**
+ * @brief Right shift all LEDs by one position
+ */
+void WS2812_BufRightShift(void) {
+  u8 r_bit0 = ws2812_buf[(WS2812_NUM - 1) * 3 + 0];
+  u8 r_bit1 = ws2812_buf[(WS2812_NUM - 1) * 3 + 1];
+  u8 r_bit2 = ws2812_buf[(WS2812_NUM - 1) * 3 + 2];
+  for (uint8_t i = WS2812_NUM - 1; i > 0; i--) {
+    ws2812_buf[i * 3 + 0] = ws2812_buf[(i - 1) * 3 + 0];
+    ws2812_buf[i * 3 + 1] = ws2812_buf[(i - 1) * 3 + 1];
+    ws2812_buf[i * 3 + 2] = ws2812_buf[(i - 1) * 3 + 2];
+  }
+  ws2812_buf[0] = r_bit0;
+  ws2812_buf[1] = r_bit1;
+  ws2812_buf[2] = r_bit2;
+}
+
+/**
+ * @brief Left shift all LEDs by one position
+ */
+void WS2812_BufLeftShift(void) {
+  u8 l_bit0 = ws2812_buf[0];
+  u8 l_bit1 = ws2812_buf[1];
+  u8 l_bit2 = ws2812_buf[2];
+  for (uint8_t i = 0; i < WS2812_NUM - 1; i++) {
+    ws2812_buf[i * 3 + 0] = ws2812_buf[(i + 1) * 3 + 0];
+    ws2812_buf[i * 3 + 1] = ws2812_buf[(i + 1) * 3 + 1];
+    ws2812_buf[i * 3 + 2] = ws2812_buf[(i + 1) * 3 + 2];
+  }
+  ws2812_buf[(WS2812_NUM - 1) * 3 + 0] = l_bit0;
+  ws2812_buf[(WS2812_NUM - 1) * 3 + 1] = l_bit1;
+  ws2812_buf[(WS2812_NUM - 1) * 3 + 2] = l_bit2;
+}
+
+/**
+ * @brief Flip the buffer
+ */
+void WS2812_BufFlip(void) {
+  for (uint8_t i = 0; i < WS2812_NUM / 2; i++) {
+    uint8_t r_bit0 = ws2812_buf[i * 3 + 0];
+    uint8_t r_bit1 = ws2812_buf[i * 3 + 1];
+    uint8_t r_bit2 = ws2812_buf[i * 3 + 2];
+    ws2812_buf[i * 3 + 0] = ws2812_buf[(WS2812_NUM - i - 1) * 3 + 0];
+    ws2812_buf[i * 3 + 1] = ws2812_buf[(WS2812_NUM - i - 1) * 3 + 1];
+    ws2812_buf[i * 3 + 2] = ws2812_buf[(WS2812_NUM - i - 1) * 3 + 2];
+    ws2812_buf[(WS2812_NUM - i - 1) * 3 + 0] = r_bit0;
+    ws2812_buf[(WS2812_NUM - i - 1) * 3 + 1] = r_bit1;
+    ws2812_buf[(WS2812_NUM - i - 1) * 3 + 2] = r_bit2;
+  }
+}
---
+++ DriversMcu/STM32F407/Drivers/Drv_WS2812.h
@@ -0,0 +1,65 @@
+/**
+ * @file Drv_WS2812.h
+ * @author Ellu (lutaoyu@163.com)
+ * @version 1.0
+ * @date 2022-06-11
+ *
+ * THINK DIFFERENTLY
+ */
+
+#ifndef _DRV_WS2812_H_
+#define _DRV_WS2812_H_
+#include "Drv_Sys.h"
+#include "SysConfig.h"
+
+// PIN defination
+#define __WS2812_RCC RCC_AHB1Periph_GPIOD
+#define __WS2812_GPIO GPIOD
+#define __WS2812_PIN GPIO_Pin_12
+////
+
+#define WS2812_NUM 8
+
+#define __WS2812_HIGH __WS2812_GPIO->BSRRL = __WS2812_PIN
+#define __WS2812_LOW __WS2812_GPIO->BSRRH = __WS2812_PIN
+
+////
+#define __DELAY_700NS \
+  for (uint8_t i = 0; i < 18; i++) __NOP()
+#define __DELAY_600NS \
+  for (uint8_t i = 0; i < 10; i++) __NOP()
+//
+#define __DELAY_350NS \
+  for (uint8_t i = 0; i < 8; i++) __NOP()
+#define __DELAY_800NS \
+  for (uint8_t i = 0; i < 15; i++) __NOP()
+//
+#define __WS2812_HIGH_BIT \
+  __WS2812_HIGH;          \
+  __DELAY_700NS;          \
+  __WS2812_LOW;           \
+  __DELAY_600NS
+#define __WS2812_LOW_BIT \
+  __WS2812_HIGH;         \
+  __DELAY_350NS;         \
+  __WS2812_LOW;          \
+  __DELAY_800NS
+////
+
+#define __2812_WAIT MyDelayUs(70)
+#define __2812_SET(d, t) \
+  WS2812_SendBit((uint8_t*)d, sizeof(d) / sizeof(uint8_t) / 3, t)
+
+void DrvWS2812Init(void);
+void WS2812_SendBit(uint8_t* data, uint8_t len);
+void WS2812_SetARGB(uint32_t argb_data, uint8_t index);
+void WS2812_SetRGB(uint32_t rgb_data, uint8_t index);
+void WS2812_SetAll(uint32_t argb_data);
+void WS2812_BufRightShift(void);
+void WS2812_BufLeftShift(void);
+void WS2812_BufFlip(void);
+void WS2812_ForceSendBuf(void);
+void WS2812_SendBuf(void);
+void WS2812_SendBufChecker(void);
+
+#endif
