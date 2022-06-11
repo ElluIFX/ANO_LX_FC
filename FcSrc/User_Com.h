55cc84b57705d2f4fb42eeb3f3867ca304d6dc90
Ellu <31679332+ElluIFX@users.noreply.github.com>
2022-06-11 17:22:11 +0800

    🦄 refactor(用户通信): 完全重构用户通信协议

--- FcSrc/User_Com.h
+++ FcSrc/User_Com.h
@@ -4,7 +4,7 @@
  * @author Ellu (lutaoyu@163.com)
  * @version 1.0
  * @date 2022-06-08
- * 
+ *
  * THINK DIFFERENTLY
  */
 
@@ -13,9 +13,37 @@
 
 #include "SysConfig.h"
 
+#define USER_DATA_EXCHANGE_TIMEOUT_S (0.05f - 0.001f)
+#define USER_HEARTBEAT_TIMEOUT_S (1.0f - 0.001f)
+
+//回传数据结构
+typedef struct {
+  u8 head1;
+  u8 head2;
+  u8 length;
+  s16 rol_x100;
+  s16 pit_x100;
+  s16 yaw_x100;
+  s32 alt_fused;
+  s16 vel_x;
+  s16 vel_y;
+  s16 vel_z;
+  s32 pos_x;
+  s32 pos_y;
+  u16 voltage_100;
+  u8 fc_mode_sta;
+  u8 unlock_sta;
+  u8 CID;
+  u8 check_sum;
+} __attribute__((__packed__)) _to_user_st;
+
+typedef union {
+  u8 byte_data[33];
+  _to_user_st st_data;
+} _to_user_un;
+
+
 void UserCom_GetOneByte(u8 data);
 
-void UserCom_DataAnl(u8 *data_buf, u8 data_len);
-
-
+void UserCom_Task(float dT_s);
 #endif
