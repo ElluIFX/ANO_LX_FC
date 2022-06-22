757a80107519f6c919d6173f2edb3942ac7d13f9
Ellu <31679332+ElluIFX@users.noreply.github.com>
2022-06-22 19:37:39 +0800

    🎈 perf(logger): 简化不必要的那一层类

--- python_sdk/FlightController/Logger.py
+++ python_sdk/FlightController/Logger.py
@@ -5,73 +5,55 @@ import sys
 import colorlog  # 控制台日志输入颜色
 
 
-class Fc_Log:
-    def __init__(self):
-        log_colors_config = {
-            "DEBUG": "cyan",
-            "INFO": "bold_green",
-            "WARNING": "bold_yellow",
-            "ERROR": "bold_red",
-            "CRITICAL": "bold_purple",
-        }
-        text_colors_config = {
-            "DEBUG": "cyan",
-            "INFO": "white",
-            "WARNING": "bold_yellow",
-            "ERROR": "bold_red",
-            "CRITICAL": "bold_purple",
-        }
-        self.logger = logging.getLogger()
+def get_logger():
+    log_colors_config = {
+        "DEBUG": "cyan",
+        "INFO": "bold_green",
+        "WARNING": "bold_yellow",
+        "ERROR": "bold_red",
+        "CRITICAL": "bold_purple",
+    }
+    text_colors_config = {
+        "DEBUG": "cyan",
+        "INFO": "white",
+        "WARNING": "bold_yellow",
+        "ERROR": "bold_red",
+        "CRITICAL": "bold_purple",
+    }
+    logger = logging.getLogger()
 
-        path = os.path.dirname(os.path.realpath(sys.argv[0]))
-        logFile = os.path.join(path, "fc_log.log")
-        self.logger.setLevel(logging.DEBUG)
+    path = os.path.dirname(os.path.realpath(sys.argv[0]))
+    logFile = os.path.join(path, "fc_log.log")
+    logger.setLevel(logging.DEBUG)
 
-        fmtConsole = colorlog.ColoredFormatter(
-            "\n\n%(log_color)s[%(levelname)s] %(asctime)s %(0_log_color)s%(message)s",
-            "%Y-%m-%d %H:%M:%S",
-            log_colors=log_colors_config,
-            secondary_log_colors={0: text_colors_config},
-        )
-        fmtFile = logging.Formatter(
-            "[%(levelname)s] %(asctime)s\n%(message)s",
-            "(%Y-%m-%d %H:%M:%S)",
-        )
-        self.logger.fileHdl = logging.FileHandler(logFile, encoding="utf-8")
-        self.logger.consoleHdl = logging.StreamHandler()
-        self.logger.fileHdl.setLevel(logging.DEBUG)
-        self.logger.consoleHdl.setLevel(logging.INFO)
-        self.logger.fileHdl.setFormatter(fmtFile)
-        self.logger.consoleHdl.setFormatter(fmtConsole)
-        self.logger.addHandler(self.logger.fileHdl)
-        self.logger.addHandler(self.logger.consoleHdl)
-
-    def debug(self, msg, *args, **kwargs):
-        self.logger.debug(msg, *args, **kwargs)
-
-    def info(self, msg, *args, **kwargs):
-        self.logger.info(msg, *args, **kwargs)
-
-    def warning(self, msg, *args, **kwargs):
-        self.logger.warning(msg, *args, **kwargs)
-
-    def error(self, msg, *args, **kwargs):
-        self.logger.error(msg, *args, **kwargs)
-
-    def critical(self, msg, *args, **kwargs):
-        self.logger.critical(msg, *args, **kwargs)
-
-    def set_level(self, level):
-        levelList = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
-        if level in levelList:
-            self.logger.setLevel(level)
+    fmtConsole = colorlog.ColoredFormatter(
+        "\n\n%(log_color)s[%(levelname)s] %(asctime)s %(0_log_color)s%(message)s",
+        "%Y-%m-%d %H:%M:%S",
+        log_colors=log_colors_config,
+        secondary_log_colors={0: text_colors_config},
+    )
+    fmtFile = logging.Formatter(
+        "[%(levelname)s] %(asctime)s\n%(message)s",
+        "(%Y-%m-%d %H:%M:%S)",
+    )
+    logger.fileHdl = logging.FileHandler(logFile, encoding="utf-8")
+    logger.consoleHdl = logging.StreamHandler()
+    logger.fileHdl.setLevel(logging.DEBUG)
+    logger.consoleHdl.setLevel(logging.INFO)
+    logger.fileHdl.setFormatter(fmtFile)
+    logger.consoleHdl.setFormatter(fmtConsole)
+    logger.addHandler(logger.fileHdl)
+    logger.addHandler(logger.consoleHdl)
+    return logger
 
 
-logger = Fc_Log()
+logger = get_logger()
 
 
 def print(msg, *args, **kwargs):
-    logger.info(msg, *args, **kwargs)
+    for a in args:
+        msg += f" {a}"
+    logger.info(msg)
 
 
 def Exception_Catcher(func):
@@ -86,24 +68,3 @@ def Exception_Catcher(func):
             raise e
 
     return wrapper
-
-
-if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        logger.info(str(sys.argv[1]))
-    if len(sys.argv) > 2:
-        typeDict = {
-            "-d": logger.debug,
-            "-i": logger.info,
-            "-w": logger.warning,
-            "-e": logger.error,
-            "-c": logger.critical,
-        }
-        line = ""
-        try:
-            for i in sys.argv[2:]:
-                line += str(i) + " "
-            typeDict[sys.argv[1].lower()](line)
-        except:
-            line = str(sys.argv[1]) + " " + line
-            logger.info(line)
