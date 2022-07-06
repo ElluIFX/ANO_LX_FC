457f8ee584432ed7c120fd53c4ce08f819b1f632
Ellu <31679332+ElluIFX@users.noreply.github.com>
2022-07-06 20:36:23 +0800

    完成起飞阶段

--- python_sdk/mission1.py
+++ python_sdk/mission1.py
@@ -11,6 +11,9 @@ class Mission(object):
     def __init__(self, fc: FC_Controller, radar: LD_Radar):
         self.fc = fc
         self.radar = radar
+        ############### PID #################
+        self.height_pid = PID(0.1, 0.0, 0.0, setpoint=100, output_limits=(-20, 20))
+        self.height_pid.set_auto_mode(False)
 
     def run(self):
         ############### 参数 #################
@@ -19,9 +22,9 @@ class Mission(object):
         camera_up_pwm = 72
         camera_up_45_pwm = 91.75
         set_buzzer = lambda x: self.fc.set_digital_output(0, x)
-        #####################################
         fc = self.fc
         radar = self.radar
+        fc.set_PWM_output(0, camera_up_pwm)
 
         logger.info("[MISSION] Mission Started")
         fc.unlock()
@@ -31,4 +34,8 @@ class Mission(object):
         sleep(2)
         fc.set_height(1, 100, 30)
         fc.wait_for_hovering()
+        radar.start_find_point(
+            timeout=1, type=0, from_=-45, to=45, num=1, range_limit=1500
+        )
+        ######## 已起飞
         sleep(2)
