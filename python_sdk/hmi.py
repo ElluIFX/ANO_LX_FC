75ba5343ed0b10345dfdec9c310c4be853c4ef58
Ellu <31679332+ElluIFX@users.noreply.github.com>
2022-07-27 18:51:37 +0800

    新增串口屏

---
+++ python_sdk/hmi.py
@@ -0,0 +1,32 @@
+# 陶晶驰串口屏驱动
+import time
+
+import serial
+
+
+class HMI(object):
+    def __init__(self, port, baudrate: int = 115200):
+        self._serial = serial.Serial(port, baudrate)
+        self._buffer = bytes()
+        self._timer = time.time()
+        self._flag = False
+
+    def command(self, data: str):
+        send = data.encode("gbk") + b"\xFF\xFF\xFF"
+        self._serial.write(send)
+
+    def read(self, timeout: float = 0.01) -> str:
+        if self._serial.in_waiting > 0:
+            if not self._flag:
+                self._flag = True
+                self._buffer = bytes()
+            self._buffer += self._serial.read(self._serial.in_waiting)
+            self._timer = time.time()
+        if self._flag and time.time() - self._timer > timeout:
+            self._flag = False
+            data = self._buffer.decode()
+            return data
+        return None
+
+    def info(self, data: str):
+        self.command(f'info.txt="{data}"')
